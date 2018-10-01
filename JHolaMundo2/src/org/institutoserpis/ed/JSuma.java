2c6780bca3260628a836ba38ea28ca45c719a130
aarpeiDAM <1dam.apalmer@institutoserpis.org>
2018-10-01 09:38:25 +0200

    Clase suma añadida

---
+++ JHolaMundo2/src/org/institutoserpis/ed/JSuma.java
@@ -0,0 +1,15 @@
+package org.institutoserpis.ed;
+import java.util.Scanner;
+
+public class JSuma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner tcl = new Scanner(System.in);
+		float num1 = tcl.nextFloat();
+		float num2 = tcl.nextFloat();
+		float suma = num1+num2;
+		System.out.println(suma);
+	}
+
+}
