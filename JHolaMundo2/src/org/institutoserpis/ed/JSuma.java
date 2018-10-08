da70723fe9aa07cf734679ba5ecf1d0d2649fb3e
aarpeiDAM <1dam.apalmer@institutoserpis.org>
2018-10-08 09:20:12 +0200

    Añadido JSuma

--- JHolaMundo2/src/org/institutoserpis/ed/JSuma.java
+++ JHolaMundo2/src/org/institutoserpis/ed/JSuma.java
@@ -4,12 +4,10 @@ import java.util.Scanner;
 public class JSuma {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner tcl = new Scanner(System.in);
 		float num1 = tcl.nextFloat();
 		float num2 = tcl.nextFloat();
-		float suma = num1+num2;
-		System.out.println(suma);
+		System.out.println(num1+num2);
 	}
 
 }
