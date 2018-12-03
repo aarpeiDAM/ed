aeb68fcbb93de3629ad27ff3dc57624b73f3bf35
aarpeiDAM <1dam.apalmer@institutoserpis.org>
2018-12-03 08:11:04 +0100

    Ordenar Vector MonoDevelop

---
+++ CVector/CVector/CVector/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CVector
+{
+    class Vector
+    {
+        public static void Main(string[] args)
+        {
+            int[] orderedArray = { 12, 13, 33, 36, 47, 52, 74,95};
+            Console.WriteLine(BinarySearch(orderedArray,12));
+  
+        }
+        public static int BinarySearch(int[] v, int x){
+            int indexOfX = v.Length/2;
+            if (v.Length == 0)
+                return -1;
+            for(int w = 0; w < v.Length; w++){
+                if (v[indexOfX] == x)
+                {
+                    return indexOfX;
+
+                }
+                else if (v[indexOfX] < x)
+                {
+                    if (indexOfX / 2 == 0)
+                        indexOfX += 1;
+                    else
+                        indexOfX += indexOfX / 2;
+                }
+                else
+                {
+                    if (indexOfX / 2 == 0)
+                        indexOfX -= 1;
+                    else
+                        indexOfX -= indexOfX / 2;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
---
+++ CVector/CVector/CVector/Properties/VectorTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+namespace CVector.Properties
+{
+    [TestFixture()]
+    public class VectorTest
+    {
+        [Test()]
+        public void BinarySearch()
+        {
+            Assert.AreEqual(-1,Vector.BinarySearch(new int[]{},33));
+            Assert.AreEqual(0, Vector.BinarySearch(new int[] {12,15,16,19,20}, 12));
+            Assert.AreEqual(4, Vector.BinarySearch(new int[] { 10, 25, 30, 60, 65}, 65));
+            Assert.AreEqual(2, Vector.BinarySearch(new int[] { 12, 26, 45, 36, 96}, 45));
+            Assert.AreEqual(3, Vector.BinarySearch(new int[] { 35, 38, 95, 99, 100}, 99));
+            Assert.AreEqual(2, Vector.BinarySearch(new int[] { 12, 12, 12, 12,12}, 12));
+        }
+    }
+}
