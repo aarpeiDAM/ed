f0d4b9f159a40a6b9cff19c69f992118ba4cd5dd
aarpeiDAM <1dam.apalmer@institutoserpis.org>
2018-12-03 09:55:53 +0100

    Añadida Calculadora hecha co GTK

---
+++ CSumaGTK/CSumaGTK/MainWindow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        suma.Clicked+=delegate {
+            result.Show();
+            int resultado = Convert.ToInt32(entryOne.Text) + Convert.ToInt32(entryTwo.Text);
+            result.Text = Convert.ToString(resultado);
+        };
+        resta.Clicked += delegate {
+            result.Show();
+            int resultado = Convert.ToInt32(entryOne.Text) - Convert.ToInt32(entryTwo.Text);
+            result.Text = Convert.ToString(resultado);
+        };
+        division.Clicked += delegate {
+            result.Show();
+            double resultado = Convert.ToDouble(entryOne.Text) / Convert.ToDouble(entryTwo.Text);
+            result.Text = Convert.ToString(resultado);
+        };
+        multiplicacion.Clicked += delegate {
+            result.Show();
+            int resultado = Convert.ToInt32(entryOne.Text) * Convert.ToInt32(entryTwo.Text);
+            result.Text = Convert.ToString(resultado);
+        };
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
