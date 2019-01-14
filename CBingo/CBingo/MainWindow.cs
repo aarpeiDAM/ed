504ddf8f313cd29f228cf607a887988008bad3e0
aarpeiDAM <1dam.apalmer@institutoserpis.org>
2019-01-14 08:08:57 +0100

    Bingo separado en clases

---
+++ CBingo/CBingo/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+using System.Collections.Generic;
+using CBingo;
+
+public partial class MainWindow : Gtk.Window
+{
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        Panel panel = new Panel(vBox);
+        Bombo bombo = new Bombo();
+
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
