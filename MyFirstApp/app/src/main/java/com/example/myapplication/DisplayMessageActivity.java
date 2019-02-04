48cda9124a5f9f31c69f0c3e0457d9562541e201
aarpeiDAM <1dam.apalmer@institutoserpis.org>
2019-02-04 08:03:32 +0100

    Proyecto de android studio terminado

---
+++ MyFirstApp/app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        String message = getIntent().getStringExtra("clave1");
+
+    }
+
+}
--- MyFirstApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static String message="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view){
+        EditText editText =findViewById(R.id.editText);
+        String message =editText.toString();
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        intent.putExtra("clave1",message);
+        startActivity(intent);
+
     }
 }
