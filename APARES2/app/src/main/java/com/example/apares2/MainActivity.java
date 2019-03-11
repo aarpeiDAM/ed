f609984c30df5ab757b58e2273ea60c5413c5ca4
aarpeiDAM <1dam.apalmer@institutoserpis.org>
2019-03-11 08:32:37 +0100

    Proyecto Android Studio Terminado con código simplificado

--- APARES2/app/src/main/java/com/example/apares2/MainActivity.java
+++ APARES2/app/src/main/java/com/example/apares2/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.apares;
+package com.example.apares2;
 
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -7,21 +7,17 @@ import android.view.View;
 import android.widget.Button;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public String palabraClave="";
     public List<String> listaPalabras= new ArrayList<String>();
-    public List<Button> listaBotonesVacios= new ArrayList<Button>();
-    public List<Button> listaBotonesRellenos= new ArrayList<Button>();
-    public List<String> listaBotonesRellenos2= new ArrayList<String>();
     public TextView textView;
     public TextView textView2;
     public int actualScore=0;
-    public int botonC=0;
-    public int indexTarget=0;
+
     Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,57 +47,45 @@ public class MainActivity extends AppCompatActivity {
         listaPalabras.add("Cuero");
         listaPalabras.add("Trigo");
         listaPalabras.add("Zanahorias");
+
+        Collections.shuffle(listaPalabras);
+
         for (int i = 1; i < 13; i++) {
             int id = getResources().getIdentifier("button"+i, "id", getPackageName());
             Button btn = findViewById(id);
-            listaBotonesVacios.add(btn);
+            btn.setText(listaPalabras.get(i-1));
         }
-        int random=establecerRandom(listaPalabras.size());
-        palabraClave=listaPalabras.get(random);
-        textView.setText(palabraClave);
-        listaPalabras.remove(random);
-        random=establecerRandom(listaBotonesVacios.size());
-        listaBotonesVacios.get(random).setText(palabraClave);
-        listaBotonesRellenos.add(listaBotonesVacios.get(random));
-        botonC=listaBotonesRellenos.get(0).getId();
-        listaBotonesVacios.remove(random);
-        for (int i = 0; i < 11; i++) {
-            random=establecerRandom(listaPalabras.size());
-            String palabraAPoner=listaPalabras.get(random);
-            listaPalabras.remove(random);
-            random=establecerRandom(listaBotonesVacios.size());
-            listaBotonesVacios.get(random).setText(palabraAPoner);
-            listaBotonesRellenos.add(listaBotonesVacios.get(random));
-            listaBotonesVacios.remove(random);
-        }
-        for(int i = 0; i<listaBotonesRellenos.size();i++){
-            listaBotonesRellenos2.add(listaBotonesRellenos.get(i).getText().toString());
-        }
+
+        Collections.shuffle(listaPalabras);
+        textView.setText(listaPalabras.get(0));
+
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 12; i++) {
-                    listaBotonesRellenos.get(i).setText("");
-
+                for (int i = 1; i < 13; i++) {
+                    int id = getResources().getIdentifier("button"+i, "id", getPackageName());
+                    Button btn = findViewById(id);
+                    btn.setTextSize(0);
                 }
             }
         }, 5000);
 
+
+
     }
     public void comprobar(View view){
         Button b = (Button)view;
-        int buttonPos = b.getId();
-        if(buttonPos==botonC){
+        if(b.getText()==listaPalabras.get(0)){
             textView.setText("Correcto");
             actualScore+=1;
             if(actualScore!=12){
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    actualizarScore();
-                }
-            }, 2000);}
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        actualizarScore();
+                    }
+                }, 2000);}
             else{
                 textView2.setText("Score: "+Integer.toString(actualScore));
                 textView.setText("Has ganado");
@@ -109,35 +93,25 @@ public class MainActivity extends AppCompatActivity {
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        listaBotonesRellenos.clear();
-                        listaBotonesRellenos2.clear();
-                        listaBotonesVacios.clear();
                         listaPalabras.clear();
                         actualScore=0;
-                        botonC=0;
-                        indexTarget=0;
-                        palabraClave="";
                         textView2.setText("Score: "+Integer.toString(actualScore));
                         ponerPalabrasEnBotones();
                     }
                 }, 5000);
 
             }
-            }
         }
+    }
 
     public void actualizarScore(){
         textView2.setText("Score: "+Integer.toString(actualScore));
         cambiarTarget();
-        textView.setText(listaBotonesRellenos2.get(indexTarget));
+        textView.setText(listaPalabras.get(0));
 
     }
     public void cambiarTarget(){
-        listaBotonesRellenos2.remove(indexTarget);
-        listaBotonesRellenos.remove(indexTarget);
-        indexTarget=establecerRandom(listaBotonesRellenos2.size());
-        botonC=listaBotonesRellenos.get(indexTarget).getId();
+        listaPalabras.remove(0);
 
     }
 }
-
