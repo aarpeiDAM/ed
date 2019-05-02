f61680040e2481085659805710cb678c9fd7c3b5
aarpeiDAM <1dam.apalmer@institutoserpis.org>
2019-05-02 12:21:55 +0200

    CProyectoAgenda Empezado

--- CProyectoAgenda/CProyectoAgenda/CProyectoAgenda/MainWindow.cs
+++ CProyectoAgenda/CProyectoAgenda/CProyectoAgenda/MainWindow.cs
@@ -6,11 +6,11 @@ public partial class MainWindow : Gtk.Window
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
         Build();
-        treeview1.AppendColumn("Nombre",new CellRendererText(),"text", 0);
-        treeview1.AppendColumn("Teléfono", new CellRendererText(), "text",1);
-        ListStore listStore= new ListStore(typeof(String), typeof(String));
+        treeview1.AppendColumn("Nombre", new CellRendererText(), "text",0);
+        treeview1.AppendColumn("Teléfono", new CellRendererText(), "text", 1);
+        ListStore listStore = new ListStore(typeof(String), typeof(String));
         treeview1.Model = listStore;
-        listStore.AppendValues("Eiron","54678856");
+        listStore.AppendValues("Eiron", "67457567");
     }
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
