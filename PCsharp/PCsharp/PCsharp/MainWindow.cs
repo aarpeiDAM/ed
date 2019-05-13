ddeefeec6df0afcad21e9013fe464c4d17ee78ec
aarpeiDAM <1dam.apalmer@institutoserpis.org>
2019-05-13 08:37:52 +0200

    Commit antes de fusionar ramas

--- PCsharp/PCsharp/PCsharp/MainWindow.cs
+++ PCsharp/PCsharp/PCsharp/MainWindow.cs
@@ -6,6 +6,14 @@ public partial class MainWindow : Gtk.Window
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
         Build();
+        treeview1.AppendColumn("Nombre", new CellRendererText(), "text", 0);
+        treeview1.AppendColumn("Teléfono", new CellRendererText(), "text", 1);
+        ListStore listStore = new ListStore(typeof(String), typeof(String));
+        treeview1.Model = listStore;
+        listStore.AppendValues("Eiron", "67457567");
+        editAction.Sensitive = false;
+        table1.Sensitive = false;
+        newAction.Activate
     }
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
